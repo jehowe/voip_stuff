7e7b796de89d7a593fdc3bc5f50b5fc1dccf836a
Jeff Howe <jehowe@users.noreply.github.com>
2021-09-28 18:33:44 -0500

    Update bulkvs-webhook-sms-relay.php
    Cleaned up request validation for efficiency

--- bulkvs-webhook-sms-relay.php
+++ bulkvs-webhook-sms-relay.php
@@ -10,19 +10,13 @@
 // Script will process and parse any SMS sent to your webhook from BulkVS
 // and forward the text to a number of your choosing.
 
-// Process requests sending data - BulkVS sends request data using POST in JSON format
-$json = file_get_contents('php://input');
+//----- Begin Script --------//
 
-// Kill script if it's an empty request
-if (empty($json)) {
-  exit();
-}
 
-// Decode the json data
-$decoded_json = json_decode($json);
+//----- Request validation - two steps --------//
 
-// Validate request is coming from bulkvs gateway IP's:  199.255.157.195 or 69.12.88.195 currently
-// Get requester's IP address
+// Step 1: Verify request is coming from bulkvs gateway IP's: 199.255.157.195 or 69.12.88.195 currently
+// Get remote IP
 if (!empty($_SERVER['HTTP_CLIENT_IP']))
   {
     $ip_address = $_SERVER['HTTP_CLIENT_IP'];
@@ -35,31 +29,44 @@ else
   {
     $ip_address = $_SERVER['REMOTE_ADDR'];
   }
-
+// Kill session if request is not from valid bulkvs IP's
 if (($ip_address != "199.255.157.195") && ($ip_address != "69.12.88.195")) {
-  // Address is not from bulkvs, exit
+  // Request is not from bulkvs, exit
   exit();
 }
 
-// Request validated - parse the SMS 'to', 'from', and 'message' into variables from the json data
+// Collect request data - BulkVS sends request data using POST in JSON format
+$json = file_get_contents('php://input');
+
+// Step 2: Kill script if it's an empty request
+if (empty($json)) {
+  exit();
+}
+
+//----- Process request data --------//
+
+// Decode the json data
+$decoded_json = json_decode($json);
+
+// Parse incoming SMS 'To', 'From', and 'Message' json data into variables
 $to = $decoded_json->To[0];
 $from = $decoded_json->From;
 $message = $decoded_json->Message;
-// decode any special characters to readable format
+// Decode any special characters if found in the message to readable format
 $message = urldecode($message);
 
-// Construct relay message
+// Construct outbound SMS relay message
 $sms_text = "From: ".$from." Message: ".$message;
 
-// Construct relay data payload with bulkvs keys, to, from, and message details
+// Construct outbound SMS relay data into json payload with bulkvs keys, to, from, and message details
 $payload = json_encode( array(	"apikey" => "<bulkvs apikey>",
 	"apisecret" => "<bulkvs secret>",
-	"from" => "<11-digit bulkvs number sms will be sent from>",
+	"from" => $to,
 	"to" => "<11-digit recipient number>",
-	"message" => $sms_text,
+	"message" => $sms_text
 ) );
 
-// Construct bulkvs SMS URL and forward the message using bulkvs gateway
+// Construct bulkvs SMS URL and forward the message using curl to the bulkvs gateway
 $ch = curl_init( "https://portal.bulkvs.com/sendSMS" );
 curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
 curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
